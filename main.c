5186324d498e9fb27d8e93fd748aafd055a3583b
M.Ghelichkhani <m.ghelichkhani@ebidar.net>
2025-06-09 09:50:47 +0330

    initial project

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void  vulnerable_function() {
+    char name[16];
+    int password = 0;
+
+    printf("enter your name: ");
+    gets(name);
+
+    if(password == 1337)
+        puts("ok");
+    else
+        puts("nok");
+}
+
+int main() {
+
+    vulnerable_function();
+
+
+
+    //system("calc");
+    //system("notepad");
+    //system("code .");
+    return 0;
+}
